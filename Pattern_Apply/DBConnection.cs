989028ae026ed5fa7403dde0d089efa5d4a5e349
SokunPanha <sokunpanha.panha@gmail.com>
2024-04-02 08:15:02 +0700

    Update DBConnection.cs

--- Pattern_Apply/DBConnection.cs
+++ Pattern_Apply/DBConnection.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -6,40 +6,59 @@ namespace Pattern_Apply
 {
     internal class DBConnection
     {
-
-        // Create an instance of SqlDatabaseFactory
-        private readonly SqlDatabaseStrategy sqlDatabase;
+        private static DBConnection instance;
+        private DatabaseString databaseString;
         private string connectionString;
-
         private Sql sql;
         private SqlAdapter sql_adapter;
-
         private DatabaseInteraction database;
-        private static DBConnection? instance;
 
-        private DBConnection(string DBType)
+        private DBConnection(DatabaseType DBType)
         {
-            this.sqlDatabase = new SqlDatabaseStrategy();
-            this.connectionString = this.sqlDatabase.CreateConnectionString();
+            this.databaseString = new DatabaseString();
+            this.connectionString = this.databaseString.CreateConnection(DBType);
             this.sql = new Sql(connectionString);
             this.sql_adapter = new SqlAdapter(this.sql);
             this.database = new DatabaseInteraction();
         }
 
-        public static DBConnection GetInstance(string DBType)
+        public static DBConnection GetInstance(DatabaseType DBType)
         {
-           
-                if (instance == null)
-                {
-                    instance = new DBConnection(DBType);
-                }
-                return instance;
-            
+            if (instance == null)
+            {
+                instance = new DBConnection(DBType);
+            }
+            return instance;
         }
 
-        public SqlConnection GetConnection()
+        public DataTable Query(string query)
         {
-            return (SqlConnection)database.GetConnection(this.sql_adapter);
+            DataTable dataTable = new DataTable();
+            IDbConnection connection = database.GetConnection(sql_adapter);
+            try
+            {
+                connection.Open();
+                IDbCommand command = connection.CreateCommand();
+                command.CommandText = query;
+
+                // Execute the query and fill the DataTable with the results
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    dataTable.Load(reader);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error executing query: {ex.Message}");
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            return dataTable;
         }
     }
 }
